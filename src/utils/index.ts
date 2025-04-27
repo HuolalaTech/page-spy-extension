74c89ce9c03110f482e080bda61c05c157ac5c03
Blucas <15555602203@163.com>
2025-04-27 10:52:52 +0800

    feat: update the match rules

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,5 @@ export function matchCurrentTab(rules: string, tabUrl: string) {
   return rules
     .split('\n')
     .filter((i: string) => !!i.trim())
-    .map((d: string) => new RegExp(d))
-    .some((r) => r.test(tabUrl));
+    .some((r: string) => tabUrl.includes(r) || new RegExp(r).test(tabUrl));
 }
